a505553669fc513cca2bb66fd4fcf41d03aa02b0
Vanesa Cardona <vanesacardona076@gmail.com>
2021-11-02 14:42:51 -0600

    Crear Organigrama - Arbol familiar

---
+++ js/data.js
@@ -0,0 +1,44 @@
+var data = {
+
+   "id": 1,
+  "puesto": "PAPA",
+  "nombre": "Juan Cardona",
+  "hijos": [
+    {
+      "id": 2,
+      "puesto": "MAMA",
+      "nombre": "Morena Perez",
+      "hijos": [
+        {
+          "id": 3,
+          "puesto": "YO",
+          "nombre": "K Vanesa Cardona Perez",
+          "hijos": [
+           
+          ]
+        },
+        {
+          "id": 4,
+          "puesto": "HERMANA",
+          "nombre": "X Ivonne Cardona Perez",
+          "hijos": []
+        },
+        {
+          "id": 5,
+          "puesto": "HERMANO",
+          "nombre": "Kevin J Cardona Perez",
+          "hijos": []
+        },{
+          "id": 6,
+          "puesto": "HERMANO",
+          "nombre": "J Diego Cardona Perez",
+          "hijos": []
+        }
+      ]
+    }
+    
+    
+   
+    
+  ]
+}
